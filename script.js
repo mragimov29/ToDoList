81ca29bb94d7d373bf2e838b337864a33e80762d
mragimov29 <mragimov29@gmail.com>
2022-11-08 00:07:32 +0400

    add images folder & change js

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-let form = document.querySelector('.form');
-let sortButton = document.querySelector('.sort');
-let addButton = document.querySelector('.add');
-let removeLi = document.getElementsByClassName('remove-li');
-let formHeight = 30, height = 246, counter = 0, myClass = 0;
-let dragged, id, index, indexDrop, list, removeButton, inputLine, input;
-
-addButton.addEventListener('click', (e) => {
-    let div = document.createElement('div');
-    div.innerHTML = `<input placeholder="Введите текст" type="text" class="task-${myClass}" maxlength="25">
-    <img src="/images/remove.svg" class="remove" alt="remove">`;
-    div.classList.add('input-line');
-    form.append(div);
-
-    input = document.querySelectorAll('input');
-    removeButton = document.querySelectorAll('.remove');
-    inputEnter();
-    if (counter == 4) {
-        form.style.overflow = 'scroll';
-        form.style.overflowX = 'hidden';
-    }
-    counter++;
-    form.scrollTop = form.scrollHeight;
-});
-
 function inputEnter() {
     input.forEach(item => {
         item.addEventListener('keyup', (event) => {
             event.preventDefault();
-            if (event.key == 'Enter') {
+            if (event.key == 'Enter')
                 item.readOnly = true;
-
-                if (counter >= 4)
-                    form.scrollTop = form.scrollHeight;
-                if (inputLine.length == 0)
-                    myClass = 0;
-            }
         });
 
         removeButton.forEach(button => {
@@ -49,7 +18,31 @@ function inputEnter() {
             });
         });
     });
-}
+}``
+
+let form = document.querySelector('.form'),
+    sortButton = document.querySelector('.sort'), 
+    addButton = document.querySelector('.add');
+let counter = 0;
+let removeButton, input;
+
+addButton.addEventListener('click', (e) => {
+    let div = document.createElement('div');
+    div.innerHTML = `<input placeholder="Введите текст" type="text" class="task-${myClass}" maxlength="25">
+    <img src="/images/remove.svg" class="remove" alt="remove">`;
+    div.classList.add('input-line');
+    form.append(div);
+
+    input = document.querySelectorAll('input');
+    removeButton = document.querySelectorAll('.remove');
+    inputEnter();
+    if (counter == 4) {
+        form.style.overflow = 'scroll';
+        form.style.overflowX = 'hidden';
+    } else counter++;
+    form.scrollTop = form.scrollHeight;
+});
+
 
 sortButton.addEventListener('click', (event) => {
     let input = document.querySelectorAll('input');
