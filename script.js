4df1ffbf5fb21170d2e0a61cd4d487fa7102f6b0
mragimov29 <mragimov29@gmail.com>
2022-11-09 05:17:11 +0400

    change 4k styles

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let form = document.querySelector('.form'),
 sortButton = document.querySelector('.sort'), 
 addButton = document.querySelector('.add');
-let removeButton, input;
+let removeButton, input, inputLine, items, tasksListElement = document.querySelector(`.tasks-list`), counter = 0;
 
 addButton.addEventListener('click', (e) => {
     let div = document.createElement('div');
-    div.innerHTML = `<input placeholder="Введите текст" type="text" class="task" maxlength="25">
+    div.innerHTML = `<input placeholder="Введите текст" type="text" maxlength="25">
     <img src="/images/remove.svg" class="remove" alt="remove">`;
     div.classList.add('input-line');
+    div.draggable = true;
     form.append(div);
-
+    let li = document.createElement('li');
+    document.querySelector('ul').append(li);
+    li.append(div);
+    li.draggable = true;
+    li.classList.add('task');
+    
+    form = document.querySelector('.form');
     input = document.querySelectorAll('input');
     removeButton = document.querySelectorAll('.remove');
     inputEnter();
@@ -28,6 +35,7 @@ function inputEnter() {
             event.preventDefault();
             if (event.key == 'Enter') {
                 item.readOnly = true;
+                dragAndDrop();
             }
         });
 
@@ -43,7 +51,64 @@ function inputEnter() {
             });
         });
     });
+
+    
 }
+function dragAndDrop() {
+    let items = document.getElementsByTagName("li"), current = null;
+
+  // (B) MAKE ITEMS DRAGGABLE + SORTABLE
+  for (let i of items) {
+    // (B1) ATTACH DRAGGABLE
+    i.draggable = true;
+    
+    // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
+    i.ondragstart = (ev) => {
+      current = i;
+      for (let it of items) {
+        if (it != current) { it.classList.add("hint"); }
+      }
+    };
+    
+    // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
+    i.ondragenter = (ev) => {
+      if (i != current) { i.classList.add("active"); }
+    };
+
+    // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
+    i.ondragleave = () => {
+      i.classList.remove("active");
+    };
+
+    // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
+    i.ondragend = () => { for (let it of items) {
+        it.classList.remove("hint");
+        it.classList.remove("active");
+    }};
+ 
+    // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
+    i.ondragover = (evt) => { evt.preventDefault(); };
+ 
+    // (B7) ON DROP - DO SOMETHING
+    i.ondrop = (evt) => {
+      evt.preventDefault();
+      if (i != current) {
+        let currentpos = 0, droppedpos = 0;
+        for (let it=0; it<items.length; it++) {
+          if (current == items[it]) { currentpos = it; }
+          if (i == items[it]) { droppedpos = it; }
+        }
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling);
+        } else {
+          i.parentNode.insertBefore(current, i);
+        }
+      }
+    };
+  }
+}
+
+
 
 sortButton.addEventListener('mouseover', (event) => {
     if(event.target.id == 'up')
